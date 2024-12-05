2e3e05c2bda89071b320b41e7458d86313fd9d87
ddunger <davordunger@gmail.com>
2024-12-05 18:30:26 +0100

    Add project files.

---
+++ Ispit.API/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Ispit.API.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Ispit.API
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Item> ShoppingItem { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=ShoppingDb;Integrated Security = True;TrustServerCertificate = True;");
+
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
---
+++ Ispit.API/Controllers/ItemsController.cs
@@ -0,0 +1,102 @@
+﻿using Ispit.API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ispit.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ItemsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Items
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Item>>> GetShoppingItem()
+        {
+            return await _context.ShoppingItem.ToListAsync();
+        }
+
+        // GET: api/Items/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Item>> GetItem(int id)
+        {
+            var item = await _context.ShoppingItem.FindAsync(id);
+
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            return item;
+        }
+
+        // PUT: api/Items/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutItem(int id, Item item)
+        {
+            if (id != item.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(item).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ItemExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Items
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Item>> PostItem(Item item)
+        {
+            _context.ShoppingItem.Add(item);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetItem", new { id = item.Id }, item);
+        }
+
+        // DELETE: api/Items/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteItem(int id)
+        {
+            var item = await _context.ShoppingItem.FindAsync(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            _context.ShoppingItem.Remove(item);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ItemExists(int id)
+        {
+            return _context.ShoppingItem.Any(e => e.Id == id);
+        }
+    }
+}
